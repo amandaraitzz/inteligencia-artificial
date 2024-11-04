7e931aaae4e1e3c82c8958e58eba2354433f0729
amandaraitzz <110539985+amandaraitzz@users.noreply.github.com>
2024-11-04 08:56:34 -0300

    criaçao de perguntas

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa__principal");
+const caixaPerguntas = document.querySelector(".caixa__perguntas");
+const caixaAlternativas = document.querySelector(".caixa__alternativas");
+const caixaResultados = document.querySelector(".caixa__resultados");
+const textoResultado = document.querySelector(".texto__resultado");
+
+const pergunta = [
+    {
+    enunciado:"Pergunta 1"
+    alternativas : [ 
+        "Alternativa 1";
+        "Alternativa 2"
+    ] 
+    },
+    {
+        enunciado:"Pergunta 2"
+        alternativas : [ 
+            "Alternativa 1";
+            "Alternativa 2"
+        ] 
+        }, 
+];
