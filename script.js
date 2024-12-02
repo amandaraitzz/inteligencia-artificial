c1a0ca43d338478803005b5a7b71b681e5bda4ed
amandaraitzz <110539985+amandaraitzz@users.noreply.github.com>
2024-12-02 09:38:34 -0300

    .

--- script.js
+++ script.js
@@ -4,19 +4,45 @@ const caixaAlternativas = document.querySelector(".caixa__alternativas");
 const caixaResultados = document.querySelector(".caixa__resultados");
 const textoResultado = document.querySelector(".texto__resultado");
 
-const pergunta = [
+const perguntas = [
     {
-    enunciado:"Pergunta 1"
-    alternativas : [ 
-        "Alternativa 1";
-        "Alternativa 2"
-    ] 
+        enunciado: "Sofia é uma jovem estudante de música, apaixonada pelo piano. Desde pequena, ela sonha em tocar em grandes palcos, mas também é fascinada por compor suas próprias melodias. Recentemente, ela recebeu duas oportunidades únicas que podem mudar sua vida. Agora, ela precisa tomar uma decisão. É uma manhã ensolarada, e Sofia está sentada à beira do lago, segurando duas cartas que chegaram pelo correio. Uma delas é de uma prestigiada escola de música, oferecendo a ela uma bolsa para estudar performance pianística. A outra é de uma gravadora independente, interessada em suas composições. Sofia sabe que ambas as escolhas são incríveis, mas não pode fazer as duas ao mesmo tempo. Ela respira fundo e decide qual caminho seguir.",
+        alternativas: [
+            "Aceitar a bolsa da escola de música",
+            "Assinar com a gravadora independente"
+        ]
     },
     {
-        enunciado:"Pergunta 2"
-        alternativas : [ 
-            "Alternativa 1";
-            "Alternativa 2"
-        ] 
-        }, 
+        enunciado: "Ao entrar na academia, Sofia se depara com alunos talentosos e exigentes professores. Em sua primeira aula, a professora pede para ela tocar algo que a represente. Sofia sente o peso da responsabilidade.",
+        alternativas: [
+            "Tocar uma peça clássica que ela domina.",
+            "Arriscar e tocar uma composição própria."
+        ]
+    },
+    {
+        enunciado: "A gravadora quer lançar um álbum com suas músicas, mas propõe alterações que Sofia acredita mudarem a essência de suas composições.",
+        alternativas: [
+            "Aceitar as mudanças para agradar o mercado.",
+            "Insistir em manter suas músicas autênticas."
+        ]
+    },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
